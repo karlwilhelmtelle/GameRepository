19ca3b3b3d7b3dcea589000781caeb70e9d8808e
Karl Wilhelm Telle <karl.wilhelm.telle@gmail.com>
2016-10-10 10:29:53 +0200

    Revert "header dateien und figure klasse"
    This reverts commit 72e4965e19740b8d7b17302b9f2ac1d98cf2b163.

--- GameProject/GameProject/main.cpp
+++ GameProject/GameProject/main.cpp
@@ -1,46 +1,26 @@
-#include <main.h>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <vector>
+#include <string>
 
-int main() 
-{
+int main() {
 
 	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Game", sf::Style::Fullscreen);
 	window.setMouseCursorVisible(false);
 
 	sf::CircleShape shape;
-	shape.setRadius(40);
+	shape.setRadius(40.f);
 	shape.setPosition(600, 600);
 	shape.setFillColor(sf::Color::Cyan);
 
-	while (window.isOpen()) 
-	{
-		sf::Event event;
-		while (window.pollEvent(event)) 
-		{
-			if (event.type == sf::Event::Closed)
-			{
-				window.close();
-			}
-		}
+	while (window.isOpen()) {
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		{
-			shape.move(0, -1);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		{
-			shape.move(0, +1);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		{
-			shape.move(+1, 0);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		{
-			shape.move(-1, 0);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			window.close();
+		sf::Event event;
+		while (window.pollEvent(event)) {
+
+			if (event.type == sf::Event::Closed)
+				window.close();
 		}
 
 		window.clear();
--- GameProject/GameProject/main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <vector>
-#include <string>
