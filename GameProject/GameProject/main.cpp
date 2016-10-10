72e4965e19740b8d7b17302b9f2ac1d98cf2b163
Karl Wilhelm Telle <karl.wilhelm.telle@gmail.com>
2016-10-10 10:29:22 +0200

    header dateien und figure klasse

--- GameProject/GameProject/main.cpp
+++ GameProject/GameProject/main.cpp
@@ -1,26 +1,46 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <vector>
-#include <string>
+#include <main.h>
 
-int main() {
+int main() 
+{
 
 	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Game", sf::Style::Fullscreen);
 	window.setMouseCursorVisible(false);
 
 	sf::CircleShape shape;
-	shape.setRadius(40.f);
+	shape.setRadius(40);
 	shape.setPosition(600, 600);
 	shape.setFillColor(sf::Color::Cyan);
 
-	while (window.isOpen()) {
-
+	while (window.isOpen()) 
+	{
 		sf::Event event;
-		while (window.pollEvent(event)) {
-
+		while (window.pollEvent(event)) 
+		{
 			if (event.type == sf::Event::Closed)
+			{
 				window.close();
+			}
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			shape.move(0, -1);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			shape.move(0, +1);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			shape.move(+1, 0);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			shape.move(-1, 0);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
 		}
 
 		window.clear();
---
+++ GameProject/GameProject/main.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <vector>
+#include <string>
